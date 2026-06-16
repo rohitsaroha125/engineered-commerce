216297b92c92be067bfd88ad993aecee05c2da6b
rohitsaroha125 <rohitsaroha125@gmail.com>
2026-06-16 22:34:24 +0530

    First Commit

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to backend");
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
