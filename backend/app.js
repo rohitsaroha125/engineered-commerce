93b3d004af77e0a92d3e23c93977b17c25b332a3
rohitsaroha125 <rohitsaroha125@gmail.com>
2026-06-17 08:12:54 +0530

    Fix: Docker compose file

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8010;
 
 app.get("/", (req, res) => {
   res.send("Welcome to backend");
